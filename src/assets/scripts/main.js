602486d8d32be638651d20ce44e635677aea0fcb
Elicia Durtnall <eliciadurtnall@gmail.com>
2014-12-07 12:37:03 -0500

    Initial Setup

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,243 @@
+
+// -----------------------------
+// Wait for DOM Load
+// -----------------------------
+
+jQuery(function($) {
+
+  // -----------------------------
+  // Router
+  // -----------------------------
+
+  var Router = Backbone.Router.extend({
+
+    // Our Routes
+    routes: {
+      '' : 'home',
+      'about': 'about',
+      'portfolio': 'portfolio',
+      'contact': 'contact'
+    },
+
+    // Home Route
+    home: function() {
+      console.log('Navigating to Home Page');
+      App.views['home'].render();
+    },
+
+    // About Route
+    about: function() {
+      console.log('Navigating to About Page');
+      App.views['about'].render();
+    },
+    // Portfolio Route
+    portfolio: function() {
+      console.log('Navigating to Portfolio Page');
+      App.views['portfolio'].render();
+    },
+
+
+    // Contact Route
+    contact: function() {
+      console.log('Navigating to Contact Page');
+      App.views['contact'].render();
+    }
+
+  });
+
+  // -----------------------------
+  // Application
+  // -----------------------------
+
+  var Application = function() {
+
+    // Add router
+    this.router = new Router();
+
+    // Setup views
+    this.views = {
+      home: new HomeView(),
+      about: new AboutView(),
+      portfolio: new PortfolioView(),
+      contact: new ContactView()
+    };
+
+  };
+
+  // -----------------------------
+  // Home View
+  // -----------------------------
+
+  var HomeView = Backbone.View.extend({
+
+    // Our Container Element
+    el: $('.main'),
+
+    // Our template ID
+    template: '#home',
+
+    // Initialize View
+    initialize: function() {
+
+      // Setup our template and start our model
+      this.template = Handlebars.compile($(this.template).html());
+      this.model = new Backbone.Model({});
+
+      // Some page data
+      this.model.set({
+        content: '<div class="intro"><h1>Elicia Durtnall / UX Design</h1><h2>"Enthusiastic, Web Design and Interactive Media graduate. UX designer.</h2></div>'
+
+      });
+
+    },
+
+    // Our Render Function
+    render: function() {
+
+      // Get data and render our template
+      var data = this.model.toJSON();
+      var html = this.template(data);
+
+      // Set update the containers HTML
+      $(this.el).html(html);
+    }
+
+  });
+
+  // -----------------------------
+  // About View
+  // -----------------------------
+
+  var AboutView = Backbone.View.extend({
+
+    // Our Container Element
+    el: $('.main'),
+
+    // Our template ID
+    template: '#about',
+
+    // Initialize View
+    initialize: function() {
+
+      // Setup our template and start our model
+      this.template = Handlebars.compile($(this.template).html());
+      this.model = new Backbone.Model({});
+
+      // Some page data
+      this.model.set({
+        content: '<h1>About Page</h1><p>Hi, my name is Elicia Durtnalltwitter@eliciad. I look forward to hearing from you! </p>'
+      });
+
+    },
+
+    // Our Render Function
+    render: function() {
+
+      // Get data and render our template
+      var data = this.model.toJSON();
+      var html = this.template(data);
+
+      // Set update the containers HTML
+      $(this.el).html(html);
+    }
+
+  });
+ // -----------------------------
+  // Portfolio View
+  // -----------------------------
+
+  var PortfolioView = Backbone.View.extend({
+
+    // Our Container Element
+    el: $('.main'),
+
+    // Our template ID
+    template: '#portfolio',
+
+    // Initialize View
+    initialize: function() {
+
+      // Setup our template and start our model
+      this.template = Handlebars.compile($(this.template).html());
+      this.model = new Backbone.Model({});
+
+      // Some page data
+      this.model.set({
+        content: '<h1>Portfolio Page</h1>'
+      });
+
+    },
+
+    // Our Render Function
+    render: function() {
+
+      // Get data and render our template
+      var data = this.model.toJSON();
+      var html = this.template(data);
+
+      // Set update the containers HTML
+      $(this.el).html(html);
+    }
+
+  });
+
+  // -----------------------------
+  // Contact View
+  // -----------------------------
+
+  var ContactView = Backbone.View.extend({
+
+    // Our Container Element
+    el: $('.main'),
+
+    // Our template ID
+    template: '#contact',
+
+    // Initialize View
+    initialize: function() {
+
+      // Setup our template and start our model
+      this.template = Handlebars.compile($(this.template).html());
+      this.model = new Backbone.Model({});
+
+      // Some page data
+      this.model.set({
+        content: '<h1>Contact Page</h1>'
+      });
+
+    },
+
+    // Our Render Function
+    render: function() {
+
+      // Get data and render our template
+      var data = this.model.toJSON();
+      var html = this.template(data);
+
+      // Set update the containers HTML
+      $(this.el).html(html);
+    }
+
+  });
+
+  // -----------------------------
+  // Start Application
+  // -----------------------------
+
+  var App = new Application();
+
+  // Start Backbone History
+  Backbone.history.start({ pushState: true });
+
+  // -----------------------------
+  // Navigation Links
+  // -----------------------------
+
+  $(document).delegate('a', 'click', function(e) {
+    e.preventDefault();
+    App.router.navigate($(this).attr('href'), { trigger: true });
+
+  });
+
+
+});
